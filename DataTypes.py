505f78e9277eebafa2b5ce589d8309aa502ae144
Krishna Aryal <krishna.dreamersit@gmail.com>
2022-05-05 21:14:20 +0545

    May 5, 2022

---
+++ DataTypes.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  5 20:20:32 2022
+@author: Krishna Aryal
+"""
+
+# Data Types
+# a. Basic type (bool, int, float, str)
+# i. boolean -Store only two values (True or False)
+
+# Example-1
+# var1 = False
+# print(var1)
+
+# Example-2
+# Accept boolean input from user (i.e. keyboard)
+# declare variables
+# var1 = None
+# var2 = None
+
+# input
+# var1 = input("Enter any boolean value : ") # string input
+
+# processing
+# var2 = bool(var1) # type conversion (string to boolean)
+
+# output
+# print(var2)
+
+# ii. Integer (whole number)
+# Example-3
+"""
+num1 = 1
+print(num1) # 1
+num1 = num1 + 5
+print(num1) # 6
+"""
+
+# Example-4
+# Read integer value from keyboard
+"""
+var1 = None # decalre
+var1 = input("Enter integer value : ") # input
+var1 = int(var1) # process
+var1 = var1 + 5
+print(var1) # output
+"""
+
+# iii. float (decimal number/fractional number)
+# Example-5
+"""
+var1 = 123.456
+var1 = var1 + 78
+print(var1)
+"""
+
+# Example-6
+# Read floating point number from user
+"""
+var1 = None
+var1 = input("Enter any float number : ") # string
+var1 = float(var1) # string->float
+var1 = var1 + 7
+print(var1)
+"""
+
+# iv. String (Collection of characters)
+# Example-7
+# var1 = 'Broadway'
+# var2 = "InfoSys"
+# var3 = """Nepal"""
+# print(var1, var2, var3)
+
+# Example-8
+# Read string from user
+var1 = None
+var1 = input("Enter any string ") # input-> string
+print(var1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# b. Extended type
